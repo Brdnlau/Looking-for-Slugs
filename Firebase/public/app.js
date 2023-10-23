f777d72d45a314da42ad5adff69577c4603e51b3
Granger Pasko <granger@eduroam-169-233-140-200.ucsc.edu>
2023-10-23 12:43:40 -0700

    user login popup created

---
+++ Firebase/public/app.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    try {
+      let app = firebase.app();
+      console.log(app)
+
+    } catch (e) {
+      console.error(e);
+    }
+
+  });
+
+function googleLogin(){
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+
+        .then(result => {
+            const user = result.user;
+            document.write(`Hello ${user.displayName}`);
+            console.log(user)
+        })
+        .catch(console.log)
+
+}
