627dd0a7b2fd013aa32bc4cfc775e3318611ca9c
Granger Pasko <granger@eduroam-169-233-134-150.ucsc.edu>
2023-10-30 13:45:52 -0700

    added some extra functionality AND the database to backend folder

--- Firebase/public/app.js
+++ Firebase/public/app.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // First, we ensure that we can access firebase. If not, return an error
     try {
       let app = firebase.app();
       console.log(app)
@@ -6,6 +7,22 @@ document.addEventListener('DOMContentLoaded', function() {
     } catch (e) {
       console.error(e);
     }
+    // Then, we grab the posts from our firebase. There is only one post "First Event!"
+    const db = firebase.firestore();
+    const myPost = db.collection('eventPosts').doc('FirstPost');
+    // Console log the post to see if we got it
+    console.log("Post data:" + myPost);
+    // We then access our post to grab whatever info we need
+    myPost.get()
+          .then(doc => {
+            const data = doc.data();
+            try{
+              console.log("This is the first post title: " + data.title + `<br>`)
+            }
+            catch(e){
+              console.log("It didnt work")
+            }
+          })
 
   });
 
