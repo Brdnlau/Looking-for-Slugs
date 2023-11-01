4b3163243477f576af276cd1d2447669eeb433bd
Abhay-Singh-01 <abhaysingh2887@gmail.com>
2023-10-31 17:54:51 -0700

    adding files for frontend navbar components

--- looking-for-slugs/src/App.js
+++ looking-for-slugs/src/App.js
@@ -1,12 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Login from "./components/login/login";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact/>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
