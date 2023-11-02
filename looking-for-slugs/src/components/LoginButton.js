3098dcef5b00f2a2ff792a8de58a03a1f8efcbc5
Brdnlau <Brdnlau@gmail.com>
2023-11-02 16:39:31 -0700

    added firebase initialization file and created loginbutton component with google oa

--- looking-for-slugs/src/components/LoginButton.js
+++ looking-for-slugs/src/components/LoginButton.js
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../firebase"
 
 export default function LoginButton(props){
+    const handleGoogleLogin = async () => {
+        const provider = new GoogleAuthProvider()
+        const result = await signInWithPopup(auth, provider)
+        const user = result.user
+        console.log(result.user.displayName + " logged in successfully")
+    }
     return (
-            <button className={props.class}>Register</button>
+        <button className={props.class} onClick={handleGoogleLogin}>Register</button>
     );
 }
---
+++ looking-for-slugs/src/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBmzIYxSheADRsRyc5oKq3v35mAXhATAKA",
+  authDomain: "lookingforslugs-67b9c.firebaseapp.com",
+  projectId: "lookingforslugs-67b9c",
+  storageBucket: "lookingforslugs-67b9c.appspot.com",
+  messagingSenderId: "774728549815",
+  appId: "1:774728549815:web:b5408c12ec6e5e78f8d432",
+  measurementId: "G-NEMNKT1JZD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export {auth, app};
