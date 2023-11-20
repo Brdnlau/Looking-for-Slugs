d45e78099d7030db7be8edfe098c6d1948ea1485
Abhay-Singh-01 <abhaysingh2887@gmail.com>
2023-11-19 20:21:26 -0800

    Edited Expander to show dummy map as well as format for members integration

--- looking-for-slugs/src/components/ExpandedCard.js
+++ looking-for-slugs/src/components/ExpandedCard.js
@@ -3,6 +3,10 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import './ExpandedCard.css'
 import Card from 'react-bootstrap/Card';
+import fillerImage from '../components/images/filler_image.jpg'
+import fillerImage2 from '../components/images/GoogleMapTA.webp'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function ExpandedCard(props) {
   const [show, setShow] = useState(false);
@@ -17,28 +21,67 @@ function ExpandedCard(props) {
             Expand (+)
         </p>
       </div>
-      <Modal show={show} onHide={handleClose}>
+      <Modal size="lg"show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>{props.title}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            <Card>
-                <Card.Body>
-                    <Card.Title><span class = "bold">{props.title}</span></Card.Title>
-                    <Card.Text>
-                        <span class = "bold">Owner:</span> Owner
-                    </Card.Text>
-                    <Card.Text class = "fix_margin">
-                        <span class = "bold">Time:</span> <span class = "time">{props.time}</span>
-                    </Card.Text>
-                    <Card.Text>
-                        <span class = "bold">Location:</span> {props.location}
-                    </Card.Text>
-                    <Card.Text>
-                        <span class = "bold">Description:</span> {props.content}
-                    </Card.Text>
-                </Card.Body>
-            </Card>    
+          <Card>
+              <div class = "top_section">
+                  <Card.Img variant="top" class = "top_image" src={fillerImage} />
+              </div>
+              <Card.Body>
+                  <Card.Text>
+                      <span class = "bold">Owner:</span> Owner
+                  </Card.Text>
+                  <Card.Text class = "fix_margin">
+                      <span class = "bold">Time:</span> <span class = "time">{props.time}</span>
+                  </Card.Text>
+                  <Card.Text>
+                      <span class = "bold">Location:</span> {props.location}
+                  </Card.Text>
+                  <Card.Text>
+                      <span class = "bold">Description:</span> {props.content}
+                  </Card.Text>
+                  <Card.Text>
+                      <span class = "bold">Members:</span>
+                      <Row>
+                        <Col xs={4} md={4}>
+                          member
+                        </Col>
+                        <Col xs={4} md={4}>
+                          member
+                        </Col>                        <Col xs={4} md={4}>
+                          member
+                        </Col>                        <Col xs={4} md={4}>
+                          member
+                        </Col>                        <Col xs={4} md={4}>
+                          member
+                        </Col>
+                      </Row>
+                  </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <Card.Title><span class = "bold">Map:</span></Card.Title>
+                <div class = "map">
+                  <Card.Img class = "normal-image" src={fillerImage2} />
+                </div>
+              </Card.Footer>
+            </Card>
+          {/* <Card.Body>
+              <Card.Text>
+                  <span class = "bold">Owner:</span> Owner
+              </Card.Text>
+              <Card.Text class = "fix_margin">
+                  <span class = "bold">Time:</span> <span class = "time">{props.time}</span>
+              </Card.Text>
+              <Card.Text>
+                  <span class = "bold">Location:</span> {props.location}
+              </Card.Text>
+              <Card.Text>
+                  <span class = "bold">Description:</span> {props.content}
+              </Card.Text>
+          </Card.Body> */}
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
